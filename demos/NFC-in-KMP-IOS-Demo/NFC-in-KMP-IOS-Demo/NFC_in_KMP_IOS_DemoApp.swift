9e11e207f83b90f81c355c6ef980667b6a1dacc6
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2024-05-26 12:18:27 +0200

    add demo folder

---
+++ demos/NFC-in-KMP-IOS-Demo/NFC-in-KMP-IOS-Demo/NFC_in_KMP_IOS_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFC_in_KMP_IOS_DemoApp.swift
+//  NFC-in-KMP-IOS-Demo
+//
+//  Created by didi on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NFC_in_KMP_IOS_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
